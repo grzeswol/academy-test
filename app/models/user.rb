32732899e43651619597f9de6431c9b551fba219
grzeswol <grzeswol@gmail.com>
2015-02-25 19:52:08 +0100

    Completed Task 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def years_from_now
+	  Time.now.year - date_of_birth.year if date_of_birth
+  end
 end
